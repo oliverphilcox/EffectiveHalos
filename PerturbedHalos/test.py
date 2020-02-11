e8aac63a78a0a4c030e69909643bae520836abbe
Oliver H. Philcox <ophilcox@tigercpu.princeton.edu>
2020-02-10 21:32:39 -0500

    docs

--- PerturbedHalos/test.py
+++ PerturbedHalos/test.py
@@ -7,22 +7,17 @@ class DatabaseManager(object):
     """
     Create and manage a new sqlite database.
 
-    Methods:
-        __init__: Initialize the class at a given redshift with a specified cosmology.
-        linear_power: Compute the linear power spectrum for a given k vector.
-        sigmaM: Compute sigma(M) from CLASS for input mass M.
-        sigma_logM_int: Return an interpolated value of sigma(M) corresponding to an input log10(M) grid.
-        dlns_dlogM_int: Return an interpolated value of d(ln(sigma(M)))/d(log10(M)) corresponding to an input log10(M) grid.
     """
 
-    def method(self,index):
+    def method(self,index,string):
         """
         A simple method.
 
         Args:
             input (int): an input
+            input2 (str): another input
 
         Returns:
-            1
+            int: Unity
         """
         return 1;
