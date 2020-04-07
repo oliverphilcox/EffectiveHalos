4597671fb5ea2b007b72fd56ed2e78cd60254111
Oliver H. Philcox <ophilcox@tigercpu.princeton.edu>
2020-04-07 11:15:10 -0400

    name change

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'PerturbedHalos'
+project = 'EffectiveHalos'
 copyright = '2020, Oliver H. E. Philcox'
 author = 'Oliver H. E. Philcox'
 
@@ -112,7 +112,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'PerturbedHalosdoc'
+htmlhelp_basename = 'EffectiveHalosdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -139,7 +139,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'PerturbedHalos.tex', 'PerturbedHalos Documentation',
+    (master_doc, 'EffectiveHalos.tex', 'EffectiveHalos Documentation',
      'Oliver Philcox', 'manual'),
 ]
 
@@ -149,7 +149,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'PerturbedHalos', 'PerturbedHalos Documentation',
+    (master_doc, 'EffectiveHalos', 'EffectiveHalos Documentation',
      [author], 1)
 ]
 
@@ -160,8 +160,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'PerturbedHalos', 'PerturbedHalos Documentation',
-     'Oliver Philcox', 'PerturbedHalos', 'A Fast, Physical and Fun Halo Model for the 21st Century',
+    (master_doc, 'EffectiveHalos', 'EffectiveHalos Documentation',
+     'Oliver Philcox', 'EffectiveHalos', 'A Fast, Physical and Accurate Halo Model for the 21st Century',
      'Miscellaneous'),
 ]
 
--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-     name='PerturbedHalos',
+     name='EffectiveHalos',
      version='1.0',
      author="Oliver Philcox",
      author_email="ohep2@alumni.cam.ac.uk",
      description="Combining the Halo Model and Perturbation Theory: A 1% Accurate Model to k = 1 h/Mpc",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     url="https://perturbedhalos.rtfd.io",
+     url="https://effectivehalos.rtfd.io",
      packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 2",
